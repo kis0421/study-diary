6d495f174126c86974037d1d19410b98ab2d32b7
김민규 <kig0421@gmail.com>
2021-11-20 22:05:40 +0900

    다이어리 생성용 메인페이지 추가

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,19 +1,26 @@
 import React from "react";
-import { Routes as Switch, Route } from "react-router-dom"
+import { Routes as Switch, Route, useLocation } from "react-router-dom"
 
 import NavigationMenu from "./components/NavigationMenu"
 
 import Setting from "./components/Setting"
+import Main from "./components/Main"
 
 const Routes = () => {
+  const location = useLocation();
+  console.log(location.pathname)
   return (
     <section>
-      <Switch>
-        <Route path={`/`} element={<>메인</>} />
-        <Route path={`/write`} element={<>gggg</>} />
-        <Route path={`/setting`} element={<Setting />} />
-      </Switch>
-      <NavigationMenu />
+      {location.pathname === "/"
+        ? <Main />
+        : <>
+          <Switch>
+            <Route path={`/`} element={<>메인</>} />
+            <Route path={`/write`} element={<>gggg</>} />
+            <Route path={`/setting`} element={<Setting />} />
+          </Switch>
+          <NavigationMenu />
+        </>}
     </section>)
 }
 //     <NavigationMenu />
