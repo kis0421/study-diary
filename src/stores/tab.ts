8b244f234567b0134088a7e50a060bcdecca33a6
김민규 <kig0421@gmail.com>
2021-11-20 21:51:28 +0900

    추가추가

--- src/stores/tab.ts
+++ src/stores/tab.ts
@@ -1,5 +1,6 @@
 import { observable } from "mobx"
 
+// TODO: currentTab 초기화 하는 과정 개선 필요
 export const tab = observable({
   currentTab: window.location.href.split("/#/")?.[1] || "",
   setCurrentTab(tab: "" | "write" | "setting") {
