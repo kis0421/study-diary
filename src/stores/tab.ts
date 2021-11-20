2d2df2759c10754c20893c2598c4d37750c88da4
김민규 <kig0421@gmail.com>
2021-11-20 21:30:11 +0900

    새로고침시 tab 초기화 처리

--- src/stores/tab.ts
+++ src/stores/tab.ts
@@ -1,8 +1,8 @@
 import { observable } from "mobx"
 
 export const tab = observable({
-    currentTab: "",
-    setCurrentTab(tab: "" | "write" | "setting") {
-        this.currentTab = tab
-    },
+  currentTab: window.location.href.split("/#/")?.[1] || "",
+  setCurrentTab(tab: "" | "write" | "setting") {
+    this.currentTab = tab
+  },
 })
