240d710011a04de7d8e3365bb8a2d7e478429209
김민규 <kig0421@gmail.com>
2021-11-21 20:59:26 +0900

    graphql request test

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,10 +1,23 @@
-import React from "react"
+import React, { useEffect } from "react"
+import { isRegisterdSiteId } from "../utils/graphqlBuilder"
+
 
 const Main = () => {
+
+  useEffect(() => {
+    
+  }, [])
+
+  const check = async () => {
+
+    await isRegisterdSiteId("kimminkyud");
+  }
   return (
-    <div>
-      메인입니다
-    </div>
+    <section style={{ position: "fixed", width: "100%", height: "100%" }}>
+      <div>
+        <button onClick={async () => await check()}>체크</button>
+      </div>
+    </section>
   )
 }
 
