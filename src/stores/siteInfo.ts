5c33c0be9f039a99a43588bc6be8091a91c51bb2
김민규 <kig0421@gmail.com>
2021-11-28 16:44:08 +0900

    site정보를 저장하는 store

--- src/stores/siteInfo.ts
+++ src/stores/siteInfo.ts
@@ -1,13 +1,10 @@
 import { observable } from "mobx"
-import { useParams } from "react-router-dom"
 
-const params = useParams();
-// TODO: currentTab 초기화 하는 과정 개선 필요
-const siteInfo = observable(() => ({
-    currenSiteId: params,
-    setCurrentTab(tab: "" | "write" | "setting") {
-        this.currentTab = tab
+const siteInfo = observable({
+    currenSiteId: undefined,
+    setCurrentId(id: string){
+        this.currenSiteId = id;
     },
-}))
+})
 
 export default siteInfo
