7bddf05f05aa5ec8b76372d6f04d67a01a678eab
김민규 <kig0421@gmail.com>
2021-11-28 16:44:40 +0900

    Outlet은 <Routes/>에 있는게 이상해서

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -1,7 +1,7 @@
 
 import React from "react";
 import { observer } from "mobx-react-lite";
-import { useNavigate, useParams } from "react-router-dom";
+import { useNavigate, useParams, Outlet } from "react-router-dom";
 
 import { Tabs, Tab, } from "@mui/material";
 
@@ -11,21 +11,24 @@ const NavigationMenu = () => {
   const { tab, siteInfo } = useStore();
   const param = useParams();
   const navigate = useNavigate();
+  
+  return (<>
+    <Tabs
+      style={{ position: "fixed", width: "100%", bottom: 0 }}
+      value={tab.currentTab}
+      onChange={(_, value) => {
+        tab.setCurrentTab(value);
+        navigate(`/${param.siteId}/${value}`);
+      }}
+      variant="fullWidth">
 
-  return (<Tabs
-    style={{ position: "fixed", width: "100%", bottom: 0 }}
-    value={tab.currentTab}
-    onChange={(_, value) => {
-      tab.setCurrentTab(value);
-      navigate(`/${param.siteId}/${value}`);
-    }}
-    variant="fullWidth">
+      <Tab label="내 다이어리" value={""} />
+      <Tab label="글쓰기" value={"write"} />
+      <Tab label="설정" value={"setting"} />
 
-    <Tab label="내 다이어리" value={""} />
-    <Tab label="글쓰기" value={"write"} />
-    <Tab label="설정" value={"setting"} />
-
-  </Tabs>)
+    </Tabs>
+    <Outlet />
+  </>)
 }
 
 export default observer(NavigationMenu)
