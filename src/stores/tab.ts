b68179082dc01fdefaa5270074b7ded58184883a
김민규 <kig0421@gmail.com>
2021-11-29 21:53:46 +0900

    lcation.href split 예외처리

--- src/stores/tab.ts
+++ src/stores/tab.ts
@@ -1,9 +1,8 @@
 import { observable } from "mobx"
 
 // TODO: currentTab 초기화 하는 과정 개선 필요
-
 export const tab = observable({
-  currentTab: window.location.href.split("/#/")?.[1].split("/")?.[1] || "",
+  currentTab: (window.location.href.split("/#/")?.[1] || "").split("/")?.[1] || "",
   setCurrentTab(tab: "" | "write" | "setting") {
     this.currentTab = tab
   },
