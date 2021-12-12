ff14fa98d3273a7ba49a9d0c5f8ef803ddca1ccc
김민규 <kig0421@gmail.com>
2021-12-12 18:10:49 +0900

    존하지않는 페이지에 대한 처리

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,9 +1,10 @@
 import React, { useEffect, useContext } from "react";
-import { Routes as Switch, Route } from "react-router-dom";
+import { Routes as Switch, Route, useNavigate } from "react-router-dom";
 import { observer } from "mobx-react-lite";
 import { getSiteInfo } from "./utils/graphqlBuilder"
 
 import StoreContext from "./context/StoreContext";
+import UIContext from "./context/UIContext";
 
 import NavigationMenu from "./components/NavigationMenu";
 import Setting from "./components/Setting";
@@ -12,17 +13,19 @@ import Main from "./components/Main";
 
 const Routes = () => {
   const { siteInfo } = useContext(StoreContext);
+  const { alert } = useContext(UIContext);
+  const navigate = useNavigate();
 
   useEffect(() => {
     (async () => {
       // TODO: siteIdFromLocationHref 구하는 방식에 사이드이펙트가 있는지 고려
       const siteIdFromLocationHref = window.location.href.split("/#/")?.[1].split("/")?.[0] || "";
       const currentSiteInfo = await getSiteInfo(siteIdFromLocationHref);
-
+      // TODO: 뒤로가기시 alert가 안뜨는 문제 useEffect의 deps 넣어야함
       if (currentSiteInfo.length) {
         siteInfo.setCurrentSiteInfo(currentSiteInfo[0]);
       } else {
-
+        alert({ message: "존재하지 않는 다이어리 입니다.", action: () => navigate("/") })
       }
     })()
 
