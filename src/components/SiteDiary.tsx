eaa5ee7f3559fcd5d5a7cdfeb83a6b48abfdc7fb
kis0421 <kig0421@gmail.com>
2021-12-13 23:37:47 +0900

    siteDiary 리스트 ui 추가

--- src/components/SiteDiary.tsx
+++ src/components/SiteDiary.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useContext } from "react"
 import { observer } from "mobx-react-lite";
-import { Backdrop, CircularProgress } from "@mui/material"
+import { Backdrop, CircularProgress, Paper } from "@mui/material"
 
 import { getDiaryList } from "../utils/graphqlBuilder"
 import StoreContext from "../context/StoreContext";
@@ -21,17 +21,51 @@ const SiteDiary = () => {
 
   return (<>
     <section style={{ position: "fixed", width: "100%", height: "calc(100% - 48px)", padding: "8px" }}>
-      <header style={{ fontWeight: "bold", fontSize: "1.5em", marginBottom: "8px" }}>{siteInfo.currentSiteInfo?.siteName}</header>
-      <article>
-        {(diaryInfo?.diaryList || []).map((i) => i.link)}
+      <header style={{
+        fontWeight: "bold",
+        fontSize: "1.5em",
+        paddingBottom: "8px",
+        borderBottom: "1px solid #efefef"
+      }}>{siteInfo.currentSiteInfo?.siteName}</header>
+      <article style={{
+        height: "calc(100vh - 98px)",
+        overflowY: "auto"
+      }}>
+        {(diaryInfo?.diaryList || []).map((i, index) => (
+          <div
+            key={i.idx}
+            style={{
+              float: "left",
+              display: "inline-block",
+              width: "calc(50% - 18px)",
+              marginRight: index % 2 === 0 ? "22px" : "0",
+            }}>
+            <Paper
+              style={{
+                minHeight: "250px",
+                marginBottom: "8px",
+                wordBreak: "break-all",
+                padding: "6px",
+                fontSize: "0.75em"
+              }}
+              elevation={3}>{i.link}</Paper>
+            <div style={{ marginBottom: "42px", textAlign: "center" }}>
+              <div>{i.title}</div>
+              <div style={{ fontSize: "0.8em", color: "#999" }}>{i.registerDate.substring(0, 10)}</div>
+            </div>
+          </div>
+
+        ))}
       </article>
     </section>
-    {siteInfo.currentSiteInfo === undefined &&
+    {
+      siteInfo.currentSiteInfo === undefined || diaryInfo.diaryList === undefined &&
       <Backdrop
         sx={{ color: '#fff', zIndex: (theme) => theme.zIndex.drawer + 1 }}
-        open={siteInfo.currentSiteInfo === undefined}>
+        open={siteInfo.currentSiteInfo === undefined || diaryInfo.diaryList === undefined}>
         <CircularProgress color="inherit" />
-      </Backdrop>}
+      </Backdrop>
+    }
   </>
   )
 }
