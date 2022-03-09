63377dc02714433598bca23365b13bbf9820b742
김민규 <kig0421@gmail.com>
2022-03-09 13:52:28 +0900

    KeywordInputField 기본 구현

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,6 +1,36 @@
 import React, { useEffect } from "react"
 import { getSiteInfo } from "../utils/graphqlBuilder"
 
+const KeywordInputField = (props: { setter: any }) => {
+  const [keywords, setKeywords] = React.useState([]);
+  const [keyword, setKeyword] = React.useState("");
+
+  return <div
+    style={{ display: "flex", flexWrap: "wrap" }}>
+    <input
+      type="text"
+      placeholder="키워드를 입력하세요"
+      style={{
+        fontSize: "18px",
+        color: "#555",
+        height: "18px",
+        border: "none",
+        outline: "none"
+      }}
+      onKeyPress={(e) => {
+        if (e.key === "Enter") {
+          setKeywords([...keywords, keyword])
+          setKeyword("")
+        }
+      }}
+      onChange={(e) => {
+        setKeyword(e.target.value)
+      }}
+      value={keyword}
+    />
+  </div>
+}
+
 
 const Main = () => {
 
@@ -17,6 +47,7 @@ const Main = () => {
       <div>
         <button onClick={async () => await check()}>체크</button>
       </div>
+      <KeywordInputField setter="ss" />
     </section>
   )
 }
