74563a592198f28dffa2475d97b680ff32810f9e
김민규 <kig0421@gmail.com>
2022-03-11 17:07:51 +0900

    style은 css파일에

--- src/components/KeywordInputField.tsx
+++ src/components/KeywordInputField.tsx
@@ -1,29 +1,5 @@
 
-import React, { CSSProperties, useState } from "react";
-
-const keywordInputStyle = {
-  wrap: {
-    display: "flex",
-    flexWrap: "wrap"
-  } as CSSProperties,
-  input: {
-    fontSize: "15px",
-    color: "rgb(85, 85, 85)",
-    height: "18px",
-    border: "none",
-    outline: "none",
-    paddingTop: "5px"
-  } as CSSProperties,
-  keyword: {
-    border: "1px solid black",
-    borderRadius: "12px",
-    padding: "4px 10px",
-    fontSize: "15px",
-    display: "inline-flex",
-    cursor: "pointer",
-    marginRight: "4px"
-  } as CSSProperties
-}
+import React, { useState } from "react";
 
 
 const KeywordInputField = (props: {
@@ -37,19 +13,14 @@ const KeywordInputField = (props: {
     setKeywords(new Set(Array.from(keywords).filter((i) => i !== removeTarget)))
   }
 
-  return <div
-    id="keywordInput"
-    style={{ ...keywordInputStyle.wrap }}>
-
+  return <div id="keywordInput" >
     {Array.from(keywords).map((currentKeyword, index) => <div
-      style={{ ...keywordInputStyle.keyword }}
       key={index}
       onClick={() => removeKeyword(currentKeyword)}>{currentKeyword} {"✖️"}</div>)}
 
     <input
       type="text"
       placeholder="키워드를 입력하세요"
-      style={{ ...keywordInputStyle.input }}
       onKeyPress={(e) => {
         if (e.key === "Enter" && keyword.trim() !== "") {
           setter([...Array.from(keywords), keyword])
