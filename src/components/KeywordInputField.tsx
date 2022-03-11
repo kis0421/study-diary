9f89461c06274bd9c41e90981e5b454af416361a
김민규 <kig0421@gmail.com>
2022-03-11 16:38:45 +0900

    내부 구현 Array -> Set 변경

--- src/components/KeywordInputField.tsx
+++ src/components/KeywordInputField.tsx
@@ -1,19 +1,23 @@
 
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 
 const KeywordInputField = (props: { setter: any }) => {
-  const [keywords, setKeywords] = React.useState(["키워드"]);
-  const [keyword, setKeyword] = React.useState("");
+  const [keywords, setKeywords] = useState(new Set() as Set<string>);
+  const [keyword, setKeyword] = useState("");
+
+  const removeKeyword = (removeTarget: string) => {
+    setKeywords(new Set(Array.from(keywords).filter((i) => i !== removeTarget)))
+  }
 
   return <div
     id="keywordInput">
-    {keywords.map((currentKeyword) => <div>{currentKeyword} {"✖️"}</div>)}
+    {Array.from(keywords).map((currentKeyword, index) => <div key={index} onClick={() => removeKeyword(currentKeyword)}>{currentKeyword} {"✖️"}</div>)}
     <input
       type="text"
       placeholder="키워드를 입력하세요"
       onKeyPress={(e) => {
         if (e.key === "Enter" && keyword.trim() !== "") {
-          setKeywords([...keywords, keyword])
+          setKeywords(new Set(keywords.add(keyword)))
           setKeyword("")
         }
       }}
