be4a982a281568c3fa0d5d16ff1ef2038235066f
kis0421 <kig0421@gmail.com>
2022-03-17 13:09:13 +0900

    다이어리 생성 UI

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,24 +1,47 @@
 import React, { useEffect } from "react"
+import useStore from "../useStore";
 import { getSiteInfo } from "../utils/graphqlBuilder"
-import { TextField, InputAdornment } from "@mui/material"
-import KeywordInputField from "./KeywordInputField"
-import { useSwipeable } from 'react-swipeable';
+import { TextField, InputAdornment, Button } from "@mui/material"
+
 
 const Main = () => {
-
+  const { siteInfoForm } = useStore();
+  console.log(Button === Button)
   const check = async (e: React.ChangeEvent<HTMLTextAreaElement | HTMLInputElement>) => {
     const isRegisterdSiteId = await getSiteInfo(e.target.value);
-    // console.log(isRegisterdSiteId);
   }
+
+  const textFields = [
+    { label: "다이어리 이름", type: "text" },
+    {
+      label: "아이디", helperText: "다이어리(사이트) 아이디를 입력해주세요.", type: "text",
+      InputProps: {
+        endAdornment: <InputAdornment position="start">kg</InputAdornment>,
+      },
+    },
+    { label: "비밀번호", type: "password" },
+    { label: "비밀번호 확인", type: "password" },
+  ]
 
   return (
     <section style={{ position: "fixed", width: "100%", height: "100%", }}>
-      <TextField
-        onChange={check}
-        InputProps={{
-          endAdornment: <InputAdornment position="start">kg</InputAdornment>,
-        }}
-      />
+      <h1 style={{ textAlign: "center", margin: "1.5em 0" }}>다이어리 만들기</h1>
+
+      <article style={{ textAlign: "center" }}>
+        {textFields.map((field) => <TextField
+          key={field.label}
+          label={field.label}
+          helperText={field.helperText}
+          type={field.type}
+          InputProps={field.InputProps}
+          style={{ width: "80%", marginBottom: "20px" }} />)}
+
+        <Button
+          variant="contained"
+          style={{ width: "80%", height: "48px" }}
+          onClick={() => console.log("dd")}>생성</Button>
+
+      </article>
     </section>
   )
 }
