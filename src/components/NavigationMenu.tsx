f20b4ec63c98271294fc1ec93f6896a367c2c758
김민규 <kig0421@gmail.com>
2022-03-19 15:41:38 +0900

    Navigation label은 아이콘으로

--- src/components/NavigationMenu.tsx
+++ src/components/NavigationMenu.tsx
@@ -4,6 +4,11 @@ import { observer } from "mobx-react-lite";
 import { useNavigate, useParams, Outlet } from "react-router-dom";
 
 import { Tabs, Tab, } from "@mui/material";
+import {
+  Create as CreateIcon,
+  Home as HomeIcon,
+  MoreHoriz as MoreHorizIcon,
+} from "@mui/icons-material";
 
 import useStore from "../useStore";
 
@@ -11,7 +16,7 @@ const NavigationMenu = () => {
   const { tab, siteInfo } = useStore();
   const param = useParams();
   const navigate = useNavigate();
-  
+
   return (<>
     <Tabs
       style={{ position: "fixed", width: "100%", borderTop: "1px solid #efefef", bottom: 0 }}
@@ -22,9 +27,9 @@ const NavigationMenu = () => {
       }}
       variant="fullWidth">
 
-      <Tab label="내 다이어리" value={""} />
-      <Tab label="글쓰기" value={"write"} />
-      <Tab label="설정" value={"setting"} />
+      <Tab label={<HomeIcon />} value={""} />
+      <Tab label={<CreateIcon />} value={"write"} />
+      <Tab label={<MoreHorizIcon />} value={"setting"} />
 
     </Tabs>
     <Outlet />
